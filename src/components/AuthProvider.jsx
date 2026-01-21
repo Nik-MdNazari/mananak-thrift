e24c56c8fdf8c0e3d29fda00b2214620457e8fb6
niksadoman <mdnazari@lancaster.ac.uk>
2026-01-21 19:05:21 +0800

    Wired users table from backend to Profile page - to fetch users data

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,6 +1,10 @@
 import { createContext, useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase'
+import axios from 'axios';
+import firebase from 'firebase/compat/app';
+
+const API_BASE_URL = "https://c8429e85-0cc6-41db-8186-3ad2821bb10b-00-2o2qhf461pb8k.sisko.replit.dev";
 
 export const AuthContext = createContext({
   currentUser: null,
@@ -11,8 +15,8 @@ export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null)
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+  useEffect(() =>  {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       setCurrentUser(user)
       setLoading(false)
     })
